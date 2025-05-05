f241c73b727d2b6dabdf1bfc98e9f1797ace8bcc
Borja <bgodinoconde@gmail.com>
2025-05-05 15:49:17 +0200

    Fix syntax error in index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) =; app.listen(3000); 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en puerto 3000');
+});
+
